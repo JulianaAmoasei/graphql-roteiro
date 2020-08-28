0f22f666feb8f80f889e5580137fffdb8f0f5c16
JulianaAmoasei <late.parrot@gmail.com>
2020-08-28 03:17:40 -0300

    videos 1.2 a 1.7

---
+++ api/index.js
@@ -0,0 +1,36 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const users = [
+  {
+    nome: "Ana",
+    ativo: true
+  },
+  {
+    nome: "Marcia",
+    ativo: false
+  }
+]
+
+const typeDefs = gql`
+  type User {
+    nome: String!
+    ativo: Boolean!
+  }
+
+    type Query {
+      users: [User]
+    }
+  `
+
+const resolvers = {
+  Query: {
+    users: () => users
+  },
+ };
+ 
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`Servidor rodando na porta ${url}`);
+});
